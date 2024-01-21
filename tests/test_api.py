bf234066c2f1d29d885098e524284fb025a77aa3
BratanTirex <BrtanTirex@gmail.com>
2024-01-21 21:12:33 +0300

    added 1 test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,11 @@
 from fastapi.testclient import TestClient
+
 from main import app
 
 
 client = TestClient(app)
 
 
-def test_read_item():
-    response = client.get("/")
+def test_read_main():
+    response = client.get("/api")
     assert response.status_code == 200
